652e6d563e2df3e4a111ca1b54b8dcdbbc9d66fb
Steven Prindle <flankbusiness@gmail.com>
2020-02-20 08:00:23 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-new_arr = ["kiwi"].join
+  if array.length == 1 
+    return array.first
+  elsif array.length == 2
+    array.join(" and ")
+  elsif array.length == 3
+    #return "String with commas between elements 1 and 2 and an and between elements 2 and 3, somehow using join and << I think?"
+  else array.length > 3
+    #return "The same as above, only more words haha"
+  end
 end
